bb951ced0eb26d6c74ce5b2b68bc247133ec33a6
Matt Nguyen <92770341+mnnguyen4@users.noreply.github.com>
2022-08-11 21:32:21 -0400

    Update display_character.js
    More console logging to debug entry list.

--- scripts/display_character.js
+++ scripts/display_character.js
@@ -1,6 +1,8 @@
 // Collects every entry in entry list into a list
 var entry = document.getElementsByClassName("entry");
 
+console.log(entry);
+
 // Adds onhover listener to entries in entry list
 for (let i = 0; i < entry.length; i++) {
 	entry[i].addEventListener("mouseover", display_character);
