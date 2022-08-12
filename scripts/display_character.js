a7b190632122633768bc44d415fd53966156a2d7
Matt Nguyen <92770341+mnnguyen4@users.noreply.github.com>
2022-08-12 19:35:08 -0400

    Fixed animation for big image
    Big image now moves off the right side of the screen when scrolling up to read the paragraph. Added extra lines to set right property of the big image when performing the slide in and slide out animations.
    
    Added event listener for scrolling on the page. Big image slides out when scrolled up a certain point in order to read the paragraph.

--- scripts/display_character.js
+++ scripts/display_character.js
@@ -2,6 +2,8 @@
 // creates entries in body of page
 var entries;
 var big_image;
+var offscreen;
+var curr_num;
 retrieve();
 
 // Grabs JSON file with entries
@@ -39,38 +41,63 @@ function activate_interactive() {
 		temp_img.src = "images/" + entry[i].number + ".png";
 	}
 
+	document.addEventListener("scroll", fade_big_image);
+
 	big_image = document.getElementsByClassName("big_image")[0];
+	offscreen = 1;
+}
+
+// Hides or shows big image when reaching a certain vertical point of the page
+function fade_big_image() {
+	var lastKnownScrollPosition = window.scrollY;
+
+	if (lastKnownScrollPosition < 500 && offscreen == 0) {
+		play_big_image_slide_out();
+		offscreen = 1;
+	} else if (lastKnownScrollPosition >= 500 && offscreen == 1) {
+		play_big_image_slide_in();
+		offscreen = 0;
+	}
 }
 
 // Displays matching character
 function display_character() {
 
-	var num = event.target.getAttribute("number");
+	curr_num = event.target.getAttribute("number");
 	var old_image = big_image.getAttribute("src");
-	var new_image = "images/" + num + ".png";
+	var new_image = "images/" + curr_num + ".png";
 	if (old_image != new_image) {
 
 		play_big_image_slide_in();
 		big_image.setAttribute("src", "");
-		big_image.setAttribute("src", "images/" + num + ".png");
+		big_image.setAttribute("src", "images/" + curr_num + ".png");
 
 		var bi_width = big_image.naturalWidth;
 		var bi_height = big_image.naturalHeight;
 
-		var new_height = Math.min(600, bi_height);
-		var new_width = (bi_width*new_height) / bi_height;
+		var new_height = (new_width*bi_height)/bi_width;
+		var new_width = 450;
 
 		big_image.setAttribute("width", new_width);
 		big_image.setAttribute("height", new_height);
-
 	}
+	offscreen = 0;
 }
 
 function play_big_image_slide_in() {
-	document.getElementsByClassName("big_image")[0].className = "big_image";
+	big_image.className = "big_image";
 	requestAnimationFrame((time) => {
 		requestAnimationFrame((time) => {
-			document.getElementsByClassName("big_image")[0].className = "big_image changing";
+			big_image.className = "big_image changing";
+		});
+	});
+}
+
+function play_big_image_slide_out() {
+	big_image.className = "big_image";
+	requestAnimationFrame((time) => {
+		requestAnimationFrame((time) => {
+			big_image.className = "big_image changing_out";
 		});
 	});
 }
