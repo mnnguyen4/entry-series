b0087a302b601c9f2f58b270e7c732b1f123a70a
Matt Nguyen <92770341+mnnguyen4@users.noreply.github.com>
2022-08-11 20:06:08 -0400

    Change image file type
    Changed back to png from webp since browser was having issues displaying webp.
    
    Removed image data from entries in character.json.

--- scripts/display_character.js
+++ scripts/display_character.js
@@ -14,7 +14,7 @@ function display_character() {
 	var num = event.target.getAttribute("number");
 
 	big_image.setAttribute("display", "inline");
-	big_image.setAttribute("src", "images/" + num + ".webp");
+	big_image.setAttribute("src", "images/" + num + ".png");
 
 	var bi_width = big_image.naturalWidth;
 	var bi_height = big_image.naturalHeight;
