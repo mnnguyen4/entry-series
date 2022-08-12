962a86e615da74305df81e808f3759d16138acc7
Matt Nguyen <92770341+mnnguyen4@users.noreply.github.com>
2022-08-11 22:08:33 -0400

    Reimported images
    Might be an issue with overwriting pngs, so that they are not displaying

--- scripts/display_character.js
+++ scripts/display_character.js
@@ -1,8 +1,6 @@
 // Collects every entry in entry list into a list
 var entry = document.getElementsByClassName("entry");
 
-console.log(entry);
-
 // Adds onhover listener to entries in entry list
 for (let i = 0; i < entry.length; i++) {
 	entry[i].addEventListener("mouseover", display_character);
@@ -16,7 +14,6 @@ function display_character() {
 	var num = event.target.getAttribute("number");
 
 	big_image.setAttribute("src", "images/" + num + ".png");
-	console.log("images/" + num + ".png");
 
 	var bi_width = big_image.naturalWidth;
 	var bi_height = big_image.naturalHeight;
